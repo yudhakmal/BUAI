242f3f1dbe711c7f03961461b419b6a86b75306a
yudhakmal <102567740+yudhakmal@users.noreply.github.com>
2022-06-10 07:13:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def main():
  
     # Membuat Navbar
     st.markdown("""
-    <header id="header">
+    <header id="main">
      <div class=" text-center container-fluid bg-gradient rounded mx-auto" style="background-color:rgb(222, 243, 253); ">
           <img class="pt-3 text-center img-fluid" alt="icon" src="https://drive.google.com/uc?export=view&id=1D4ZtKppUv1buusCOdjG4cIcL0U6nHN6E" width="300">
           </img>
@@ -292,7 +292,7 @@ def main():
         }
     </style>
     <div id="top">
-    <a href="#header">
+    <a href="#main">
         <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-arrow-up-square-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 8.354a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 6.207V11a.5.5 0 0 1-1 0V6.207L5.354 8.354z"/>
         </svg>
